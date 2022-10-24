36433c12fe1855a6b16039939c87bfb740cf12ca
bigmosi <kinyeramo@gmail.com>
2022-10-24 23:03:22 +0300

    my reservations

--- app/controllers/v1/reservations_controller.rb
+++ app/controllers/v1/reservations_controller.rb
@@ -1,2 +1,27 @@
 class ReservationsController < ApplicationController
+  def index
+    @reservations = current_user.reservations
+    if @reservations.empty?
+        render json: { error: 'No Reservations!' }, status: 200
+    else
+        render json: @reservations
+    end
+  end
+
+  def create
+    @myreservation = current_user.reservations.new(reservation_params)
+    if @myreservation.save
+      render json: @myreservation
+    else
+        render json: { success: false, errors: 'Oops something went wrong' }, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @reservation = Reservation.find(params[:id])
+    @reservation.destroy
+  end
+
+  private
+
 end
