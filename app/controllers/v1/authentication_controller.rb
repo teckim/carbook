26a7241c837ec7ccd4fedcad0cc5caf667f4647b
BHDHakim <hakim.bhd@gmail.com>
2022-10-25 12:35:53 +0100

    Fix login issue

--- app/controllers/v1/authentication_controller.rb
+++ app/controllers/v1/authentication_controller.rb
@@ -3,7 +3,7 @@ class V1::AuthenticationController < ApplicationController
 
   # POST /auth/login
   def login
-    @user = User.find_by_username!(params[:username])
+    @user = User.find_by_username(params[:username])
     if @user&.authenticate(params[:password])
       token = JsonWebToken.encode(user_id: @user.id)
       time = Time.now + 24.hours.to_i
