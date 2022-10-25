ed34b06cf660ff4345d817ad119c63caac8b7ecb
BHDHakim <hakim.bhd@gmail.com>
2022-10-25 12:27:08 +0100

    Add username and password_digest to users table

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+  has_secure_password
+  validates :username, presence: true, uniqueness: true
+  validates :password,
+            confirmation: true,
+            length: { minimum: 6 },
+            if: -> { new_record? || !password.nil? }
+  validates :password_confirmation, presence: true
 end
