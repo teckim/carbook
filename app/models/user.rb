5c5aa4330203851edc0e9b88ee0dde6e97bf63a8
BHDHakim <hakim.bhd@gmail.com>
2022-10-25 12:59:35 +0100

    Fix merge conflicts

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,11 +8,4 @@ class User < ApplicationRecord
   validates :password_confirmation, presence: true
 
   has_many :cars
-  has_secure_password
-  validates :username, presence: true, uniqueness: true
-  validates :password,
-            confirmation: true,
-            length: { minimum: 6 },
-            if: -> { new_record? || !password.nil? }
-  validates :password_confirmation, presence: true
 end
