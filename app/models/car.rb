4205877d26313b8717639456b8c0f3b6bca12d8c
HanningtoneM <hanningtonepompyy@gmail.com>
2022-11-01 15:10:28 +0300

    Add description and price  validation

--- app/models/car.rb
+++ app/models/car.rb
@@ -3,4 +3,6 @@ class Car < ApplicationRecord
 
   validates :model, presence: true
   validates :image, presence: true
+  validates :description, presence: true, length: { maximum: 50 }
+  validates :price, presence: true, numericality: { greater_than: 0 }
 end
