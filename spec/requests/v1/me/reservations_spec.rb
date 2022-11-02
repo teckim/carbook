82909bff9ad0dbd7f69fb3287e431b13f89f5267
bigmosi <kinyeramo@gmil.com>
2022-11-02 15:29:59 +0300

    fixed ruboco error

--- spec/requests/v1/me/reservations_spec.rb
+++ spec/requests/v1/me/reservations_spec.rb
@@ -1,7 +1,6 @@
 require 'swagger_helper'
 
 RSpec.describe 'v1/me/reservations', type: :request do
-
   before :each do
     post '/users', params: {
       user: {
@@ -19,9 +18,9 @@ RSpec.describe 'v1/me/reservations', type: :request do
   describe 'GET/reservations' do
     it 'should return a list of reservations' do
       FactoryBot.create(:car, brand: 'Toyata harrier', description: 'Description',
-                                 image: 'https://cdn.pixabay.com/photo/2012/05/29/00/43/car-49278_960_720.jpg')
+                              image: 'https://cdn.pixabay.com/photo/2012/05/29/00/43/car-49278_960_720.jpg')
       FactoryBot.create(:car, id: 2, brand: 'Toyata wish', description: 'description',
-                                 image: 'https://cdn.pixabay.com/photo/2012/05/29/00/43/car-49278_960_720.jpg')
+                              image: 'https://cdn.pixabay.com/photo/2012/05/29/00/43/car-49278_960_720.jpg')
 
       FactoryBot.create(:reservation, city: 'Imphal', date: '24/08/2022', user_id: @current_user.id, car_id: 1)
       FactoryBot.create(:reservation, city: 'Delhi', date: '24/08/2022', user_id: @current_user.id, car_id: 2)
@@ -35,7 +34,8 @@ RSpec.describe 'v1/me/reservations', type: :request do
   context 'POST/reservations' do
     it 'should create a new reservation' do
       expect do
-        post '/reservations', params: { reservation: { city: 'Imphal', date: '24/08/2022', user_id: @current_user.id, car_id: 1 } }
+        post '/reservations',
+             params: { reservation: { city: 'Imphal', date: '24/08/2022', user_id: @current_user.id, car_id: 1 } }
       end.to change(Reservation, :count).by(1)
 
       expect(response).to have_http_status(200)
@@ -44,7 +44,8 @@ RSpec.describe 'v1/me/reservations', type: :request do
 
   describe 'DELETE #destroy' do
     it 'destroys the requested reservation' do
-      reservation = FactoryBot.create(:reservation, city: 'Imphal', date: '24/08/2022', user_id: @current_user.id, car_id: 1)
+      reservation = FactoryBot.create(:reservation, city: 'Imphal', date: '24/08/2022', user_id: @current_user.id,
+                                                    car_id: 1)
       expect do
         delete :destroy, params: { id: reservation.id }
       end
