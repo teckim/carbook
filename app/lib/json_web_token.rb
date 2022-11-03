ef5459c5328275d49c9800c121075fba794a184e
BHDHakim <hakim.bhd@gmail.com>
2022-11-03 13:54:48 +0100

    Fix JWT

--- app/lib/json_web_token.rb
+++ app/lib/json_web_token.rb
@@ -11,5 +11,3 @@ class JsonWebToken
     HashWithIndifferentAccess.new decoded
   end
 end
-
-5879de873587968c10659c33b02df51fe89498636826bd0b43969f90e883acdeb7c634a54d8efa41097c6bbf34184c30b651afbd33740e779d5398d2efd61bbd
