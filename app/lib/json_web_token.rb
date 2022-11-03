99ac7659a2e65211e898d46e77d6d1ef1201d207
BHDHakim <hakim.bhd@gmail.com>
2022-11-03 13:53:05 +0100

    Update secret key

--- app/lib/json_web_token.rb
+++ app/lib/json_web_token.rb
@@ -1,5 +1,5 @@
 class JsonWebToken
-  SECRET_KEY = Rails.application.secrets.secret_key_base.to_s
+  SECRET_KEY = Rails.application.secret_key_base.to_s
 
   def self.encode(payload, exp = 24.hours.from_now)
     payload[:exp] = exp.to_i
@@ -11,3 +11,5 @@ class JsonWebToken
     HashWithIndifferentAccess.new decoded
   end
 end
+
+5879de873587968c10659c33b02df51fe89498636826bd0b43969f90e883acdeb7c634a54d8efa41097c6bbf34184c30b651afbd33740e779d5398d2efd61bbd
